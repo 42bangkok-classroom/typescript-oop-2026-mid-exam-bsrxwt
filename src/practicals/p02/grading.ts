c0c53c9fa54b2045f29001229626709c9d5771d0
bsrxwt <sorawit281249@gmail.com>
2026-01-21 14:26:36 +0700

    02-1

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,18 @@
 // Write your code below
+let point = process.argv[2];
+
+let points = Number(point);
+if(points<0||points>100||Number.isNaN(points)){
+    console.log("Invalid input");
+}
+if(points>=0&&points<=49){
+    console.log("Grade is F");
+}else if(points>=50&&points<=59){
+    console.log("Grade is D");
+}else if(points>=60&&points<=69){
+    console.log("Grade is C");
+}else if(points>=70&&points<=79){
+    console.log("Grade is B");
+}else if(points>=70&&points<=79){
+    console.log("Grade is A");
+}
