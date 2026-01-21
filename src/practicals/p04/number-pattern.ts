2307cda42ebb208feff5675149ebefbf44b66eb9
bsrxwt <sorawit281249@gmail.com>
2026-01-21 15:18:20 +0700

    i hate pattern

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,17 @@
 // Write your code below
+let nums = Number(process.argv[2]);
+if(nums<0||Number.isNaN(nums)){
+    process.exit;
+}
+
+for(let row = nums;row>=1;row--){
+    let result = "";
+    for (let j = 0; j < row-1; j++)
+        {
+            result += " ";
+        }
+    for(let co = nums-row+1;co>0;co--){
+        result += co;
+    }
+    console.log(result);
+}
